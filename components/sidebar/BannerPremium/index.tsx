6d7ccfb444cbf130ef5af4f0091d8359a9f9664b
Alfon <47669262+codepurse@users.noreply.github.com>
2025-04-20 23:02:59 +0800

    Initial Update

---
+++ components/sidebar/BannerPremium/index.tsx
@@ -0,0 +1,10 @@
+export default function BannerPremium() {
+  return (
+    <div className="banner-premium">
+      <p className="banner-premium-title">Get Premium Features</p>
+      <p className="banner-premium-description">
+        Try new experience with premium features on SolowWork.
+      </p>
+    </div>
+  );
+}
--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
+import BannerPremium from "./BannerPremium";
 import General from "./General";
 import ProjectList from "./ProjectList";
 import Support from "./Support";
@@ -9,17 +10,17 @@ import Workspace from "./Workspace";
 export default function Sidebar() {
   return (
     <Container className="sidebar">
-      <Row>
+      <Row style={{ height: "100%" }}>
         <Col>
           <ProjectList />
-          <hr className="faded-line" />
-          <div className="sidebar-container">
+          <div className="sidebar-container mt-3">
             <General />
             <hr className="faded-line" />
             <Workspace />
             <hr className="faded-line" />
             <Support />
           </div>
+          <BannerPremium />
         </Col>
       </Row>
     </Container>
