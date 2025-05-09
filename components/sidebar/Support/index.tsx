19654e31fd671726c6e81ec43726341e2c5e2cfb
Alfon <47669262+codepurse@users.noreply.github.com>
2025-05-09 22:40:55 +0800

    asdasdasd

--- components/sidebar/Support/index.tsx
+++ components/sidebar/Support/index.tsx
@@ -1,36 +1,50 @@
 import { MessageCircle, Phone, Settings } from "lucide-react";
+import { useRouter } from "next/router";
 import Space from "../../space";
 
 export default function Support() {
+  const router = useRouter();
   const menuItems = [
     {
       id: 1,
       name: "Feedback",
       icon: <MessageCircle size={17} color="gray" />,
       beta: true,
+      onClick: () => {
+        router.push("/feedback");
+      },
     },
     {
       id: 2,
       name: "Help Center",
       icon: <Phone size={17} color="gray" />,
       collapsed: true,
+      onClick: () => {
+        router.push("/help-center");
+      },
     },
     {
       id: 3,
       name: "Settings",
       icon: <Settings size={17} color="gray" />,
+      onClick: () => {
+        router.push("/settings");
+      },
     },
   ];
   return (
     <div className="sidebar-menu mt-2">
       <label className="sidebar-menu-title">Support</label>
+
       <div className="sidebar-menu-container">
         {menuItems.map((item) => (
-          <Space key={item.id} gap={10} className="sidebar-menu-item">
-            <i style={{ marginTop: "-3px" }}>{item.icon}</i>
-            <label className="sidebar-menu-item-name">{item.name}</label>
-            {item.beta && <span className="beta-badge">Beta</span>}
-          </Space>
+          <div onClick={item.onClick} key={item.id}>
+            <Space gap={10} className="sidebar-menu-item">
+              <i style={{ marginTop: "-3px" }}>{item.icon}</i>
+              <label className="sidebar-menu-item-name">{item.name}</label>
+              {item.beta && <span className="beta-badge">Beta</span>}
+            </Space>
+          </div>
         ))}
       </div>
     </div>
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,12 +1,39 @@
 import { Col, Container, Row } from "react-bootstrap";
+import Space from "../components/space";
 
 export default function Settings() {
   return (
-    <Container fluid className="settings">
+    <Container className="settings">
       <Row>
         <Col>
           <div className="settings-header">
-            <p>Settings</p>
+            <p className="settings-header-title">Settings</p>
+            <p className="settings-header-subtitle">
+              Manage your user profile and preferences
+            </p>
+            <hr className="not-faded-line" />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ width: "250px", maxWidth: "250px" }}>
+          <div className="settings-content">
+            <Space gap={8} direction="column" alignItems="start">
+              <p className="settings-content-title">Account</p>
+              <p className="settings-content-title">General</p>
+              <p className="settings-content-title">Notifications</p>
+              <p className="settings-content-title">Security & Privacy</p>
+              <p className="settings-content-title">Billing</p>
+              <p className="settings-content-title">Help</p>
+            </Space>
+          </div>
+        </Col>
+        <Col lg={8}>
+          <div className="settings-content-right">
+            <p className="settings-content-title-right">Account</p>
+            <p className="settings-content-subtitle-right">
+              Settings for your account
+            </p>
           </div>
         </Col>
       </Row>
